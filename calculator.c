b7c6a01ec8125e3df2b27c116a32686a3172e589
cypher Ace621 <cypherlambano22@gmail.com>
2026-06-05 14:56:23 +0100

    I have manage to do the basic arithmetic operator calculation

---
+++ calculator.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+int arithmetic(int x, int y){
+    char operation;
+    printf("Enter an operation (+, -, *, /): ");
+    scanf(" %c", &operation);
+    switch(operation){
+        case '+':
+
+            printf("The sum is: %d\n", x + y);
+            break;
+        case '-':
+            printf("The difference is: %d\n", x - y);
+            break;
+        case '*':
+            printf("The product is: %d\n", x * y);
+            break;
+        case '/':
+            if(y != 0){
+                printf("The quotient is: %d\n", x / y);
+            }
+            else{
+                printf("Cannot divide by zero\n");
+            }
+            break;
+        default:
+            printf("Invalid operation\n");
+    }
+}
+int main(){
+    int num1, num2;
+    char operation;
+    printf("Enter a number: ");
+    scanf("%d", &num1);
+    printf("Enter another number: ");
+    scanf("%d", &num2);
+    
+    arithmetic(num1, num2);
+    return 0;
+}
